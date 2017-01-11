b9b7e143f14b9d887b52a8b1cc1727c967aed6af
Sebastian Mandl <sebastian-mandl@gmx.net>
2017-01-11 13:22:22 +0100

    ---

--- pages/templates/user_administration.php
+++ pages/templates/user_administration.php
@@ -21,7 +21,10 @@
 											  $GLOBALS["COLUMN_USER_HOUSE_NUMBER"] => $_POST["user_house_number"],
 											  $GLOBALS["COLUMN_VOICE_ID"] => $voice_id));
 	    $_SESSION[$GLOBALS["CURRENT_USER"]] = $_POST["user_email"];
+		
+		disconnect();
 	}
+	
 ?>
 
 <!doctype html>
@@ -39,21 +42,37 @@
 		</ul>
 		
 		<div class="content user_creation_content" >
+			<?php
+				default_connect();
+				
+				$editMode = isset($_GET["key"]);
+				if($editMode) {
+					$result = query("SELECT {$GLOBALS["COLUMN_USER_EMAIL"]}, {$GLOBALS["COLUMN_USER_PHONE"]}, {$GLOBALS["COLUMN_USER_PLACE"]}, {$GLOBALS["COLUMN_USER_POSTAL_CODE"]}, {$GLOBALS["COLUMN_USER_STREET"]}, {$GLOBALS["COLUMN_USER_HOUSE_NUMBER"]}, {$GLOBALS["COLUMN_USER_FIRSTNAME"]}, {$GLOBALS["COLUMN_USER_LASTNAME"]}, {$GLOBALS["COLUMN_VOICE_ID"]} 
+									 FROM {$GLOBALS["USERS_TABLE"]}
+								     WHERE {$GLOBALS["COLUMN_USER_EMAIL"]} = '{$_GET["key"]}'");
+					
+					$result = fetch_next_row($result);
+				}
+				
+				disconnect();
+			?>
+		
+		
 			<form action="user_administration.php" method="post">
 				<div class="group_box">
 					<p class="header">Allgemeines</p>
-					<table>
+					<table>					
 						<tr class="item">
 							<td class="header">Vorname:</td>
-							<td><input spellcheck="false" type="text" name="user_firstname" /></td>
+							<td><input spellcheck="false" type="text" name="user_firstname" value="<?php if($editMode) echo $result[6]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">Nachname:</td>
-							<td><input spellcheck="false" type="text" name="user_lastname" /></td>
+							<td><input spellcheck="false" type="text" name="user_lastname" value="<?php if($editMode) echo $result[7]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">E-Mail:</td>
-							<td><input spellcheck="false" type="text" name="user_email" /></td>
+							<td><input spellcheck="false" type="text" name="user_email" value="<?php if($editMode) echo $result[0]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">Passwort:</td>
@@ -61,7 +80,7 @@
 						</tr>
 						<tr class="item">
 							<td class="header">Tel.:</td>
-							<td><input spellcheck="false" type="text" name="user_phone" /></td>
+							<td><input spellcheck="false" type="text" name="user_phone" value="<?php if($editMode) echo $result[1]; ?>"/></td>
 						</tr>
 					</table>
 				</div>
@@ -72,19 +91,19 @@
 					<table>
 						<tr class="item">
 							<td class="header">Ort:</td>
-							<td><input spellcheck="false" type="text" name="user_place" /></td>
+							<td><input spellcheck="false" type="text" name="user_place" value="<?php if($editMode) echo $result[2]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">PLZ:</td>
-							<td><input spellcheck="false" type="text" name="user_postal_code" /></td>
+							<td><input spellcheck="false" type="text" name="user_postal_code" value="<?php if($editMode) echo $result[3]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">Straße:</td>
-							<td><input spellcheck="false" type="text" name="user_street" /></td>
+							<td><input spellcheck="false" type="text" name="user_street" value="<?php if($editMode) echo $result[4]; ?>" /></td>
 						</tr>
 						<tr class="item">
 							<td class="header">Hausnummer:</td>
-							<td><input spellcheck="false" type="text" name="user_house_number" /></td>
+							<td><input spellcheck="false" type="text" name="user_house_number" value="<?php if($editMode) echo $result[5]; ?>" /></td>
 						</tr>
 					</table>
 				</div>
@@ -93,7 +112,7 @@
 					<p class="header">Stimme</p>
 					<table>
 						<tr class="item">
-							<td class="header">Stimmgattung:</td>
+							<td class="header" style="width: 67%;">Stimmgattung:</td>
 							<td>
 								<select class="selection_box" name="user_voice" >
 									<!-- insert php code here: voices are fetched from database -->
@@ -101,13 +120,16 @@
 									
 										default_connect();
 										
-										$result = query("SELECT voice_display_name
+										$result00 = query("SELECT voice_display_name
 											             FROM voices;");
 										
-										while($row = fetch_next_row($result)) {
+										
+										$index = 1;
+										while($row = fetch_next_row($result00)) {
 											?>
-												<option><?php echo $row[0] ?></option>
+												<option <?php if(isset($result) and $index == $result[8]) echo "selected"; ?>><?php echo $row[0] ?></option>
 											<?php
+											$index++;
 										}
 										
 										disconnect();
@@ -124,7 +146,7 @@
 		</div>
 		
 		<div class="content user_alteration_content">
-			<?php 
+			<?php			
 				default_connect();
 				$result = query("SELECT {$GLOBALS["COLUMN_USER_EMAIL"]}, {$GLOBALS['COLUMN_USER_FIRSTNAME']}, {$GLOBALS["COLUMN_USER_LASTNAME"]}
 								 FROM {$GLOBALS['USERS_TABLE']};");
@@ -133,9 +155,9 @@
 						<div class="group_box">
 							<table>
 								<tr class="item">
-									<td class="header"><?php echo $row[0] ?></td>
-									<br />
+									<td style="width: 60%; color: rgb(150, 15, 15);" class="text"><?php echo $row[0] ?></td>
 									<td class="text"><?php echo $row[1] . " " . $row[2] ?></td>
+									<td><img name="<?php echo $row[0] ?>" class="deletionImage" alt="Delete" src="../../res/Red_Cross.png" width="20" /></td>
 								</tr>
 							</table>
 						</div>
@@ -153,8 +175,11 @@
 			}
 			
 			hideAllContentContainers();
-			$(".user_alteration_content").show(); // default setting ; display change page for users
+			$(".user_creation_content").show(); // default setting ; display change page for users
 			
+			$(".deletionImage").click(function() {
+				window.location = window.location + "?key=" + $(this).attr("name");
+			});
 		
 			$(".user_creation").click(function() {
 				hideAllContentContainers();
