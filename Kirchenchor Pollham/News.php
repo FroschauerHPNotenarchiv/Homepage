9dc0af187fab48b14445e001673cbc7433bd8fd3
Niklas der Graf <grafniklas98@gmail.com>
2017-05-31 12:59:37 +0200

    Gmkefwdl,

--- Kirchenchor Pollham/News.php
+++ Kirchenchor Pollham/News.php
@@ -149,7 +149,7 @@
      <div class="list_elements">
 
 			 <?php
-					foreach(NewsEntry::getEntryList() as $entry) {
+					foreach(array_reverse(NewsEntry::getEntryList()) as $entry) {
 						?>
 						<h4 class="element_titel"><?php echo $entry->title?></h4>
 					<?php if(getUserRole(getUserEmail()) <= $GLOBALS["ROLES_SUBADMIN"]) : ?>
